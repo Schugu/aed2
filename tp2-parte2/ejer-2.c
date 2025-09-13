d135fdb708659b82e773b96acafa55c6b105b64b
Schugu <leo.schugu@gmail.com>
2025-09-13 18:46:19 -0300

    add: ejer 2

--- tp2-parte2/ejer-2.c
+++ tp2-parte2/ejer-2.c
@@ -1,22 +1,21 @@
 /*
-  1. Un nuevo navegador de internet requiere que se agregue un módulo para guardar el historial de
-  sitios web visitados por el usuario. Para ello se almacena en una pila la siguiente información de los
-  sitios web: fecha y hora(string), url del sitio, y código de tipo de clasificación del sitio
-  (1-Empresariales 2-Gubernamentales 3-Redes Sociales). Los sitios se insertan en la pila, según el orden
-  en el cual fueron visitados.
+  2. Un sistema operativo para teléfonos móviles requiere que se agregue un módulo para guardar el
+  historial de las llamadas de los usuarios. Para ello se almacena en una pila la siguiente información
+  de las llamadas: fecha y hora(string), número de teléfono, y código de tipo de llamada (1-Entrante
+  2-Saliente). Las llamadas se insertan en la pila, según el orden en el cual fueron realizadas.
   Se requiere:
-    a) Codificar en lenguaje C, las estructuras de datos necesarias para la declaración del tipo
-      tPilaSitiosWeb adecuado.
-    b) Una función que permita visualizar el detalle de sitios web que fueron visitados según el tipo de
-      clasificación que se recibe como parámetro. Además, se pide calcular el porcentaje de visitas
-      a sitios web que coincidan con ese tipo de clasificación respecto del total sitios visitados.
-      Un ejemplo del formato de impresión solicitado es el siguiente:
-        *** Sitios web del tipo “Redes Sociales” ***
-          Fecha y hora        url
-          14.45 hs            www.facebook.com
-          16.30 hs            www.instagram.com
-      Porcentaje de visitas a sitios de “Redes Sociales” respecto del total sitios visitados: 25%
-    c) Escribir una función que permita borrar todo el historial del navegador.
+  a) Codificar en lenguaje C, las estructuras de datos necesarias para la declaración del tipo
+  tPilaLlamadas adecuado.
+  b) Una función que permita visualizar el detalle de llamadas según el código de tipo de llamada
+  que se recibe como parámetro. Además, se pide calcular el porcentaje de llamadas que
+  coincidan con ese tipo de llamada respecto del total de llamadas. Un ejemplo del formato de
+  impresión solicitado es el siguiente:
+  *** Llamadas “Entrantes” ***
+  Fecha y hora Número de teléfono
+  19.45 hs 3794 12324
+  18.30 hs 3794 34587
+  Porcentaje de llamadas “entrantes” respecto del de llamadas: 37.75%
+  c) Escribir una función que permita borrar todo el historial de llamadas.
 */
 // Librerias
 #include <ctype.h>
@@ -27,31 +26,31 @@
 
 // Constantes
 #define MAXSTRING 50
-#define MAXARRAY 3
+#define MAXARRAY 2
 
 typedef char tString[MAXSTRING];
 
 typedef struct {
   tString fechaHora;
-  tString url;
+  tString numero;
   tString codigo;
-} tSitio;
+} tLlamada;
 
 typedef struct nodo {
-  tSitio datos;
+  tLlamada datos;
   struct nodo* siguiente;
 } tNodo;
 
 // Prototipado
 // Opcion a)
 void elegirCodigoClasif(tString);
-tSitio ingresarDatos();
-tNodo* crearNodo(tSitio, tNodo*);
-void insertarNodo(tNodo**, tSitio);
+tLlamada ingresarDatos();
+tNodo* crearNodo(tLlamada, tNodo*);
+void insertarNodo(tNodo**, tLlamada);
 void push(tNodo**);
 
 // Opcion b)
-void mostrarDatos(tSitio);
+void mostrarDatos(tLlamada);
 void mostrarPila(tNodo**);
 
 // Opcion c)
@@ -64,7 +63,7 @@ void mostrarOpcionesMenu();
 void crearMenu(tNodo**);
 
 // Variables
-tString arrayCodigoClasificacion[MAXARRAY] = {"Empresariales", "Gubernamentales", "Redes Sociales"};
+tString arrayCodigoLlamada[MAXARRAY] = {"Entrante", "Saliente"};
 
 tNodo* tope;
 
@@ -77,9 +76,9 @@ int main() {
 // Funciones
 // Opcion a)
 void elegirCodigoClasif(tString codigo) {
-  printf("Elija el codigo de claisifacion: \n");
+  printf("Elija el codigo de tipo de llamada: \n");
   for (int i = 0; i < MAXARRAY; i++) {
-    printf("[%d]: %s\n", i + 1, arrayCodigoClasificacion[i]);
+    printf("[%d]: %s\n", i + 1, arrayCodigoLlamada[i]);
   }
 
   int opcion;
@@ -91,15 +90,11 @@ void elegirCodigoClasif(tString codigo) {
     printf("\n");
     switch (opcion) {
     case 1:
-      strcpy(codigo, arrayCodigoClasificacion[0]);
+      strcpy(codigo, arrayCodigoLlamada[0]);
       valido = true;
       break;
     case 2:
-      strcpy(codigo, arrayCodigoClasificacion[1]);
-      valido = true;
-      break;
-    case 3:
-      strcpy(codigo, arrayCodigoClasificacion[2]);
+      strcpy(codigo, arrayCodigoLlamada[1]);
       valido = true;
       break;
     default:
@@ -108,20 +103,20 @@ void elegirCodigoClasif(tString codigo) {
     }
   } while (!valido);
 }
-tSitio ingresarDatos() {
-  tSitio sitio;
+tLlamada ingresarDatos() {
+  tLlamada llamada;
 
   printf("Fecha y hora: ");
-  scanf(" %49[^\n]", sitio.fechaHora);
+  scanf(" %49[^\n]", llamada.fechaHora);
 
-  printf("URL del sitio: ");
-  scanf(" %49[^\n]", sitio.url);
+  printf("Numero de telefono: ");
+  scanf(" %49[^\n]", llamada.numero);
 
-  elegirCodigoClasif(sitio.codigo);
+  elegirCodigoClasif(llamada.codigo);
 
-  return sitio;
+  return llamada;
 }
-tNodo* crearNodo(tSitio sitio, tNodo* enlace) {
+tNodo* crearNodo(tLlamada llamada, tNodo* enlace) {
   tNodo* nodo;
   nodo = (tNodo*)malloc(sizeof(tNodo));
 
@@ -129,30 +124,30 @@ tNodo* crearNodo(tSitio sitio, tNodo* enlace) {
     printf("Error al asignar memoria.");
   }
 
-  nodo->datos = sitio;
+  nodo->datos = llamada;
   nodo->siguiente = enlace;
 
   return nodo;
 }
-void insertarNodo(tNodo** tope, tSitio sitio) {
+void insertarNodo(tNodo** tope, tLlamada llamada) {
   tNodo* nodo = NULL;
   if (!*tope) {
-    nodo = crearNodo(sitio, NULL);
+    nodo = crearNodo(llamada, NULL);
   } else {
-    nodo = crearNodo(sitio, *tope);
+    nodo = crearNodo(llamada, *tope);
   }
   *tope = nodo;
 }
 void push(tNodo** tope) {
-  tSitio sitio = ingresarDatos();
-  insertarNodo(tope, sitio);
+  tLlamada llamada = ingresarDatos();
+  insertarNodo(tope, llamada);
 }
 
 // Opcion b)
-void mostrarDatos(tSitio sitio) {
-  printf("Fecha y hora: %s\n", sitio.fechaHora);
-  printf("URL del sitio: %s\n", sitio.url);
-  printf("Codigo de clasificacion: %s\n", sitio.codigo);
+void mostrarDatos(tLlamada llamada) {
+  printf("Fecha y hora: %s\n", llamada.fechaHora);
+  printf("Numero de telefono: %s\n", llamada.numero);
+  printf("Codigo de clasificacion: %s\n", llamada.codigo);
 }
 void mostrarPila(tNodo** tope) {
   if (!*tope) {
@@ -161,9 +156,9 @@ void mostrarPila(tNodo** tope) {
   }
   tNodo* aux = *tope;
   int pos = 0;
-  printf("\n\n---| Historial |---\n");
+  printf("\n\n---| Llamadas |---\n");
   while (aux) {
-    printf("\n--- Sitio en posicion: %d ---\n", pos);
+    printf("\n--- llamada en posicion: %d ---\n", pos);
     mostrarDatos(aux->datos);
     aux = aux->siguiente;
     pos++;
@@ -177,13 +172,13 @@ void mostrarPorCodigo(tNodo** cabecera) {
 
   tNodo* aux = *cabecera;
   int pos = 0;
-  int cantSitios = 0;
-  printf("\n\n---| Historial (%s) |---\n", codigo);
+  int cantLlamadas = 0;
+  printf("\n\n---| Llamadas (%ss) |---\n", codigo);
   while (aux) {
     if (strcmp(aux->datos.codigo, codigo) == 0) {
       mostrarDatos(aux->datos);
       printf("\nPosicion: %d\n\n", pos);
-      cantSitios++;
+      cantLlamadas++;
     }
     aux = aux->siguiente;
     pos++;
@@ -193,8 +188,8 @@ void mostrarPorCodigo(tNodo** cabecera) {
     printf("No hay resultados...");
     return;
   }
-  float porcentaje = ((float)cantSitios / pos) * 100;
-  printf("Porcentaje de visitas a sitios de '%s' respecto del total sitios visitados: %.2f%%", codigo, porcentaje);
+  float porcentaje = ((float)cantLlamadas / pos) * 100;
+  printf("Porcentaje de llamadas '%s' respecto del total de llamadas: %.2f%%", codigo, porcentaje);
 }
 
 // Punto d)
@@ -207,13 +202,13 @@ void eliminarHistorial(tNodo** tope) {
     aux = siguiente;
   }
   *tope = NULL;
-  printf("Historial eliminado con exito!");
+  printf("Historial de llamadas eliminado con exito!");
 }
 
 // Menú
 void mostrarOpcionesMenu() {
   printf("\n\n========================| Menu |========================\n");
-  printf("[a]: Agregar sitio.\n");
+  printf("[a]: Agregar llamada.\n");
   printf("[b]: Mostrar historial.\n");
   printf("[c]: Mostrar historial por codigo de clasificacion.\n");
   printf("[d]: Eliminar historial.\n");
